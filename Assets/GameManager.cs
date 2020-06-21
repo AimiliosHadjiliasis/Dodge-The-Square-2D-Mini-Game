149472e6dd08954ff9ad7a33a5ef4b6a5ad464a3
Aimilios <aimilioshadjiliasis@gmail.com>
2020-06-21 20:18:16 +0300

    Create Matrix Effect using Coroutines: 1) In the game manager script i created a coroutine to create an slow motion animation in the end of the game 2) I remove the restart code from EndGame function 3) Then i creted an enumeration function that slows the time of the game (with some wierd code) 4) Then restart the game after some seconds

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,9 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    public float slowness = 10f;
+
     public void EndGame()
     {
-        //Reload our scene when we collide witrh an object:
+        StartCoroutine(RestartLevel());       
+    }
+
+
+    IEnumerator RestartLevel()
+    {
+        //Before 2 seconds:
+        Time.timeScale = 1f/slowness;
+        Time.fixedDeltaTime = Time.fixedDeltaTime / slowness;
+        yield return new WaitForSeconds(1f / slowness);
+        //Aftetr 2 seconds 
+
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowness;
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
